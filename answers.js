a1fad97fbbb4731674c1252ad2354dc088c9e672
Harpreet Singh <harpreet95hs@gmail.com>
2023-05-04 22:08:48 -0400

    Methods Revisited answered

--- answers.js
+++ answers.js
@@ -59,8 +59,39 @@ for (let turtle of ninjaTurtles) {
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+console.log(favMovies[8]);
 
+favMovies.sort();
+console.log(favMovies);
 
+favMovies.pop();
+console.log(favMovies);
+
+favMovies.push("Guardians of the Galaxy");
+console.log(favMovies);
+
+favMovies.reverse();
+console.log(favMovies);
+
+favMovies.shift();
+console.log(favMovies);
+
+//unshift adds a specified elements to the array and returns the new length of the array
+
+const index = favMovies.indexOf("Django Unchained");
+favMovies.splice(index, 1, "Avatar");
+console.log(favMovies);
+
+const halfIndex = Math.floor(favMovies.length / 2);
+const lastHalf = favMovies.slice(halfIndex);
+console.log(lastHalf);
+
+console.log(favMovies);
+console.log(favMovies.indexOf("Fast and Furious"));
+
+// Was able to get Fast and furious, not sure where I missed it?
+// supposed to return -1
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
